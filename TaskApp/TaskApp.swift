257007e4ec66394866c8e1ffff4b874faec5aa4c
bin549 <549288160@qq.com>
2025-08-10 10:54:10 +0800

    chore: 移除登陆页面 & 设置白天模式

--- TaskApp/TaskApp.swift
+++ TaskApp/TaskApp.swift
@@ -1,17 +1,11 @@
-//
-//  TaskApp.swift
-//  TaskApp
-//
-//  Created by Godot on 2025/8/4.
-//
-
 import SwiftUI
 
 @main
 struct TaskApp: App {
     var body: some Scene {
         WindowGroup {
-            LoginView()
+            MainTabView()
+                .preferredColorScheme(.light)
         }
     }
 }
